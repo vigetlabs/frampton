929059d370fe3baf51a3be03e101f5c582aef018
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2020-04-06 15:30:25 -0400

    Render contenteditable

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,18 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live", Socket)
+let Hooks = {}
+Hooks.ContentEditable = {
+  mounted() {
+    this.el.addEventListener("input", e => {
+      this.pushEvent('render_post', {
+        value: e.target.innerText
+      })
+    })
+  },
+}
+
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks})
 liveSocket.connect()
 
 // Import local files
