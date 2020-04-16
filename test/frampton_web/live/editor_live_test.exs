6d863b69b842730a5c110f1e556f97e7ff384fce
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2020-04-16 12:40:17 -0400

    Add multi-editor tests

--- test/frampton_web/live/editor_live_test.exs
+++ test/frampton_web/live/editor_live_test.exs
@@ -36,4 +36,43 @@ defmodule FramptonWeb.EditorLiveTest do
       |> Floki.parse_document!
       |> Floki.find("div.rendered-output")
   end
+
+  test "when two editors are viewing the same post, they see the same rendered output", %{
+    conn: conn, post_id: post_id
+  } do
+    {:ok, view_1, _html_1} = live(conn, "/editor/#{post_id}")
+    render_keydown(view_1, :render_post, %{"value" => "# Hello World!"})
+
+    {:ok, _view_2, html_2} = live(conn, "/editor/#{post_id}")
+    [
+      {"div", _attrs, [
+        {"h1", _rendered_attrs, ["Hello World!"]}
+      ]}
+    ] = html_2
+      |> Floki.parse_document!
+      |> Floki.find("div.rendered-output")
+  end
+
+  test "when two editors are viewing the same post, they see the same raw input", %{
+    conn: conn, post_id: post_id
+  } do
+    {:ok, view_1, _html_1} = live(conn, "/editor/#{post_id}")
+    render_keydown(view_1, :render_post, %{"value" => "# Hello World!"})
+
+    {:ok, _view_2, html_2} = live(conn, "/editor/#{post_id}")
+    input_div = html_2
+             |> Floki.parse_document!
+             |> Floki.find("div.markdown-entry")
+
+    assert input_div == [
+      {"div", [{"class", "markdown-entry"}], [
+        {"div",
+          [
+            {"contenteditable", "contenteditable"},
+            {"phx-hook", "ContentEditable"}
+          ],
+          ["# Hello World!"]}
+      ]}
+    ]
+  end
 end
